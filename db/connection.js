c0bcf0acaf5b47223bacd367d305c1b7688372a0
abdoww <abdow88988@gmaill.com>
2024-06-10 18:34:06 +0300

    .

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,8 @@
 // connection.js
 import {Sequelize} from 'sequelize'
 
-export const sequelize = new Sequelize('facebook','root','', 
+export const sequelize = new Sequelize('mysql://uj7jvj3myly2vfgt:NrTav2UYWTZ6RpSM3sPm@bxzk6gtuvbfxgizmxkbb-mysql.services.clever-cloud.com:3306/bxzk6gtuvbfxgizmxkbb',
 {
-    host:"localhost",
     dialect: 'mysql'
 });
 
